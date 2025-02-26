c74dc596435d5717d77fc6a3b33cf0084f370af6
Naureen Imran <naureen.imran123@yahoo.com>
2025-02-26 13:35:35 +0000

    add a new film  and display it

--- src/main/java/com/portfolio/reel_reviews/FilmController.java
+++ src/main/java/com/portfolio/reel_reviews/FilmController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FilmController {
@@ -14,11 +16,21 @@ public class FilmController {
 	}
 	private FilmService filmService;
 	
-	@RequestMapping("film-list")
+	@RequestMapping(value = "film-list", method = RequestMethod.GET)
 	public String listAllFilms(ModelMap model) {
 	List<Film>films = filmService.findAllFilms();
 		model.addAttribute("films", films);
 		return "listFilms";
+	}
+	@RequestMapping(value = "add-film", method = RequestMethod.GET)
+	public String showNewFilm() {
+		return "film";
+	}
+	@RequestMapping(value = "add-film", method = RequestMethod.POST)
+	public String addewFilm(@RequestParam String title, String review, String stars, int year, ModelMap model) {
+		filmService.addFilm(title, year, title, stars, review);
+		
+		return "redirect:film-list";
 	}
 
 }
